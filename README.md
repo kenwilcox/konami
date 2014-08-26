1b5546373b4fbfc32b940d626f919ba33f4012d6
kenwilcox <wilcoxk@gmail.com>
2014-08-25 20:55:22 -0600

    Simple Example of Konami Code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-konami
+Konami Code
 ======
 
-Konami Code
+A simple example of Konami Code, enter it and see what happens.
---
+++ scripts/konami.js
@@ -0,0 +1,34 @@
+var konami = [];
+
+var konamiCheck = function(keyCodeArray) {
+  var ret = false;
+  var count = 0;
+  var correct = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  if (keyCodeArray.length === correct.length) {
+    for(var i = 0; i < keyCodeArray.length; i++) {
+      if (keyCodeArray[i] === correct[i]) {
+        count++;
+      }
+    }
+  }
+  if (count === correct.length) {
+    ret = true;
+  }
+  
+  return ret;
+};
+
+var konamiCode = function(event) {
+  konami.push(event.keyCode);
+  // if the length is greater than what we're looking for
+  if (konami.length > 10) {
+    //shift (drop old keyCodes)
+    konami.shift();
+  }
+  
+  if (konamiCheck(konami) === true) {
+    alert("You are a Big Nerd!");
+    window.open('http://www.bignerdranch.com','_blank');
+    konami = [];
+  }
+};
